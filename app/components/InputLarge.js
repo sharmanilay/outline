67af9c1e70bc3a93522e37c886132e98b2934f1a
Tom Moor <tom.moor@gmail.com>
2021-04-04 09:57:33 -0700

    Update InputLarge.js

--- app/components/InputLarge.js
+++ app/components/InputLarge.js
@@ -3,9 +3,10 @@ import styled from "styled-components";
 import Input from "./Input";
 
 const InputLarge = styled(Input)`
-  height: 40px;
+  height: 38px;
   flex-grow: 1;
-  margin-right: 5px;
+  margin-right: 8px;
+
   input {
     height: 38px;
   }
